e5223af13753d6d88d5c0d77f244f858a61aff0a
Ana Geguchadze <anageguchadze22@gmail.com>
2024-11-28 15:47:28 +0100

    added permissions

--- delivery/views.py
+++ delivery/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import *
 from .serializers import *
 
@@ -6,13 +7,15 @@ from .serializers import *
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class ParcelViewSet(viewsets.ModelViewSet):
     queryset = Parcel.objects.all()
     serializer_class = ParcelSerializer
-
+    permission_classes = [IsAuthenticated]
 
 class DeliveryProofViewSet(viewsets.ModelViewSet):
     queryset = DeliveryProof.objects.all()
     serializer_class = DeliveryProofSerializer
+    permission_classes = [IsAuthenticated]
